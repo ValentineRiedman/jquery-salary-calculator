ee56eb72a6c68827e1370c1a79e14e325d25dd0d
Valentine Riedman <underblueskysismile@gmail.com>
2022-04-18 20:40:54 -0500

    homework

--- scripts/client.js
+++ scripts/client.js
@@ -34,36 +34,46 @@ function displayEmployees(){
     el.empty();
     let employeeSalary = 0;
     for( let i=0; i<employees.length; i++){
-    el.append( `<li> ${ employees[i].firstName }  ${ employees[i].lastName } ${ employees[i].idNumber } ${ employees[i].title } $${ employees[i].salary }
-    <button class="removeEmployeeButton" data-index="${i}">Fire</button></li>` );
-    employeeSalary += Number( employees[i].salary/12);
-    if (employeeSalary[i] > 20000){
-        overBudget();
-     }
+    el.append(`
+    <tr>
+                <td>${ employees[i].firstName }</td>
+                <td>${ employees[i].lastName }</td>
+                <td>${ employees[i].idNumber }</td>
+                <td>${ employees[i].title }</td>
+                <td>$${ employees[i].salary }</td>
+                <th><button class="removeEmployeeButton" data-index="${i}">Fire</button></th>
+              </tr>
     
+    
+    
+    `)
+    
+    
+        //el.append( `<li> ${ employees[i].firstName }  ${ employees[i].lastName } ${ employees[i].idNumber } ${ employees[i].title } $${ employees[i].salary }
+    //<button class="removeEmployeeButton" data-index="${i}">Fire</button></li>` );
+    employeeSalary += Number( employees[i].salary/12);    
 }
  
     el = $('#employeeSalaryOut');
     el.empty();
     el.append( employeeSalary.toFixed(2) );
     let displayedSalary = employeeSalary;
+    //create a red background if over 20k
      if ( displayedSalary > 20000){
         $('#employeeSalaryOut').css("background-color", "red");
+        alert('You are over budget!');
      }
+     //keep same background if below 20k
      if( displayedSalary < 20000){
-        $('#employeeSalaryOut').css("background-color", "white");
+        $('#employeeSalaryOut').css("background-color", "aqua");
      }
 }//end displayEmployees
 
 function fireEmployee(){
-    console.log('in fireEmployee');
-    //$( this ).parent().remove();
-console.log( $( this ).data( 'index' ));
+console.log( 'in fireEmployee', $( this ).data( 'index' ));
+//give an alert when employee is fired
+alert(employees[$( this ).data( 'index' )].firstName  + ' has been fired.'   );
 employees.splice( $( this ).data( 'index' ),1);
 displayEmployees();
     
 }//end fireEmployee
-
-// function overBudget(){
-//     employeeSalaryOut.css("background-color", "red");
-// }
